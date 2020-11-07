0299c429b5dbb03046af6e2c42acf5d32df85499
cchersin <cchersin@gmail.com>
2020-11-07 17:01:34 +0100

    Update 0002_lastReadedIdx.py

--- hello/migrations/0002_lastReadedIdx.py
+++ hello/migrations/0002_lastReadedIdx.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('modulename', '0001_initial'),
+        ('migrations', '0001_initial'),
     ]
 
     operations = [
