fa8a77ba3f0b8df397efda8fe1a91f2c1059877c
cchersin <cchersin@gmail.com>
2020-11-07 16:07:18 +0100

    Update urls.py

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -16,7 +16,7 @@ import hello.views
 
 urlpatterns = [
     path("", hello.views.index, name="index"),
-    path("", hello.views.scrap, name="scrap"),
+    path("scrap/", hello.views.scrap, name="scrap"),
     path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
 ]
