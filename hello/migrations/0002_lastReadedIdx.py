251fd50fd49b45edac438baf89e8267b48b746b2
cchersin <cchersin@gmail.com>
2020-11-07 16:16:04 +0100

    Update 0002_lastReadedIdx.py

--- hello/migrations/0002_lastReadedIdx.py
+++ hello/migrations/0002_lastReadedIdx.py
@@ -1,1 +1,21 @@
-last
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    lastReadedIdx = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='LastReaded',
+            fields=[
+                ('idx', models.IntegerField()),
+             ],
+        ),
+    ]
+© 20
